6b7a382998afdbb76e2ed3803f4a3fb52d759ed3
thientai2712 <thienta2712@gmail.com>
2022-10-31 08:37:23 +0700

    a

--- src/main/java/com/cg/controller/HomeController.java
+++ src/main/java/com/cg/controller/HomeController.java
@@ -1,25 +1,22 @@
 package com.cg.controller;
 
 import org.springframework.stereotype.Controller;
-<<<<<<< HEAD
-
-@Controller
-public class HomeController {
-
-
-}
-=======
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
 
 @Controller
 @RequestMapping("/home")
 public class HomeController {
 
     @GetMapping("")
-    public ModelAndView Login(){
-        return new ModelAndView("/admin/home");
+    public ModelAndView showHomePage(){
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("/admin/home");
+        return modelAndView;
     }
 }
->>>>>>> development
+
+
