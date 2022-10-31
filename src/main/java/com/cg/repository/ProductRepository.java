03823e6af2afe08a0e3aa212a8a8faf5126209ad
thien <thien.phanvan9846@gmail.com>
2022-10-31 10:23:05 +0700

    thien_dev

--- src/main/java/com/cg/repository/ProductRepository.java
+++ src/main/java/com/cg/repository/ProductRepository.java
@@ -1,8 +1,13 @@
 package com.cg.repository;
 
+import com.cg.model.Product;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
 @Repository
+@Transactional
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
--- src/main/java/com/cg/service/ProductServiceImpl.java
+++ src/main/java/com/cg/service/ProductServiceImpl.java
@@ -1,5 +1,6 @@
 package com.cg.service;
 
+import com.cg.model.Product;
 import com.cg.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
