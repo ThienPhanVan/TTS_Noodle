8cf580ea8bcbba2f2b1635e0fe0936adda30bdb2
thien <thien.phanvan9846@gmail.com>
2022-10-31 11:02:43 +0700

    th

--- src/main/java/com/cg/TtsNoodleApplication.java
+++ src/main/java/com/cg/TtsNoodleApplication.java
@@ -2,8 +2,9 @@ package com.cg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class TtsNoodleApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/cg/repository/ProductRepository.java
+++ src/main/java/com/cg/repository/ProductRepository.java
@@ -1,10 +1,6 @@
 package com.cg.repository;
 
 import com.cg.model.Product;
-<<<<<<< HEAD
-=======
-import org.springframework.context.annotation.Bean;
->>>>>>> thien_dev
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,8 +9,4 @@ import javax.transaction.Transactional;
 @Repository
 @Transactional
 public interface ProductRepository extends JpaRepository<Product, Long> {
-<<<<<<< HEAD
-
-=======
->>>>>>> b7c1f520bb3b8cf55dab9d535d905e9d5f62136d
 }
