8b313d08a8dc983be7b701f040800fcecf14f78e
thien <thien.phanvan9846@gmail.com>
2022-11-11 13:39:33 +0700

    thien_dev

--- src/main/java/com/cg/repositories/model/Order.java
+++ src/main/java/com/cg/repositories/model/Order.java
@@ -1,55 +1,7 @@
 package com.cg.repositories.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.experimental.Accessors;
+import javax.persistence.Entity;
 
-import javax.persistence.*;
-import java.math.BigDecimal;
-import java.time.Instant;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
-@Table(name = "orders")
-@Accessors(chain = true)
 public class Order {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false)
-    private Long id;
-
-    @Column(name = "grand_total", nullable = false, precision = 12)
-    private BigDecimal grandTotal;
-
-    @Column(name = "user_id", nullable = false, insertable = false, updatable = false)
-    private Long userId;
-
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "user_id")
-    private User user;
-
-    @Column(name = "order_status")
-    private OrderStatus orderStatus;
-
-    @Column(name = "address", nullable = false, length = 95)
-    private String address;
-
-    @Column(name = "created_by", nullable = false)
-    private Long createdBy;
-
-    @Column(name = "created_at")
-    private Instant createdAt;
-
-    public Order(Long id) {
-        this.id = id;
-    }
-
-    public Order setUserId(Long userId) {
-        this.user = new User(this.userId = userId);
-        return this;
-    }
-
 }
--- src/main/java/com/cg/repositories/model/Product.java
+++ src/main/java/com/cg/repositories/model/Product.java
@@ -1,32 +1,7 @@
 package com.cg.repositories.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import javax.persistence.Entity;
 
-import lombok.experimental.Accessors;
-
-import javax.persistence.*;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Accessors(chain = true)
 @Entity
-@Table(name = "products")
- public class Product {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false)
-    private Long id;
-
-    @Column(name = "title", nullable = false, length = 55)
-    private String title;
-
-
-    public Product(long id){
-        this.id = id ;
-    }
-
-
+public class Product {
 }
