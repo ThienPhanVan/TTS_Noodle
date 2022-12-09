8f02141c0b43b156841c46ec8eea5b3eec6c55ca
thientai2712 <thienta2712@gmail.com>
2022-12-09 08:21:48 +0700

    tai_commit

--- src/main/java/com/cg/repositories/UserRepository.java
+++ src/main/java/com/cg/repositories/UserRepository.java
@@ -33,27 +33,23 @@ public interface UserRepository extends JpaRepository<User, Long> {
 //            "WHERE u.fullName LIKE %?1% " +
 //            "OR u.phone LIKE %?1% ")
 //    List<User> findUserByFullNameOrPhone(String keyword);
-
     boolean findUserByRoleId (Long roleId);
 
     @Query(value =
             "FROM User AS u " +
-                    "WHERE u.fullName LIKE %:keyword% " +
-                    "OR u.phone LIKE %:keyword% ")
+                    "WHERE u.fullName LIKE %:keyword%" +
+                    "OR u.phone LIKE %:keyword%")
     List<User> findAllByFullNameOrPhone(String keyword);
 
 
 
-    @Query(value = "FROM User AS u WHERE u.roleId = 2 and u.fullName LIKE %?1% ")
+    @Query(value = "FROM User AS u WHERE u.roleId = 2 and u.fullName LIKE %?1%")
     List<User> searchCustomer(String keyword);
 
-    @Query(value = "FROM User AS u WHERE u.roleId = 3 and u.fullName LIKE %?1% ")
+    @Query(value = "FROM User AS u WHERE u.roleId = 3 and u.fullName LIKE %?1%")
     List<User> searchSupplier(String keyword);
 
     @Query(value = "SELECT SUM(o.grandTotal) FROM  Order o where o.userId = :id")
     BigDecimal totalOrderOfUser(@Param("id") Long id);
 
-
-
-//
 }
